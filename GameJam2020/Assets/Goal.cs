b7476f0a0f99b81e720b46f60fd9bec0c6fc9cb2
Jesquive <jordan.esquivel.14@sansano.usm.cl>
2020-09-23 01:12:54 -0300

    added huawei integration, fixed ending

--- GameJam2020/Assets/Goal.cs
+++ GameJam2020/Assets/Goal.cs
@@ -6,30 +6,68 @@ public class Goal : MonoBehaviour
 {
     public Canvas joystickCanvas;
     public Canvas EndCanvas;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        // sr = GetComponent<SpriteRenderer>();
+    }
+
+    public bool startedEndingScene = false;
+    private bool buttonPressed = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log(other.CompareTag("Player"));
         if (other.CompareTag("Player"))
         {
-            if (joystickCanvas.enabled != false)
+            if (joystickCanvas.enabled != false && !startedEndingScene)
             {
+                startedEndingScene = true;
                 StartCoroutine(endkiss(other));
-
             }
         }
     }
+
+
 
     IEnumerator endkiss(Collider2D other)
     {
         other.gameObject.GetComponent<Animator>().SetTrigger("Kiss");
         gameObject.GetComponent<Animator>().SetTrigger("End");
+        other.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         joystickCanvas.enabled = false;
         AudioManager.instance.Play("Kiss");
+
+        Debug.Log(other.gameObject.GetComponent<RectangleCharacterController>() != null);
+
+        if (other.gameObject.GetComponent<RectangleCharacterController>() != null)
+        {
+            other.gameObject.GetComponent<RectangleCharacterController>().enabled = false;
+            other.gameObject.GetComponent<RectanglePlayerMovement>().enabled = false;
+
+        }
+        else if (other.gameObject.GetComponent<CharacterController>() != null)
+        {
+            other.gameObject.GetComponent<CharacterController>().enabled = false;
+            other.gameObject.GetComponent<PlayerMovement>().enabled = false;
+
+
+        }
+        yield return new WaitForSeconds(1f);
+        EndCanvas.gameObject.GetComponent<Animator>().SetTrigger("EndScene");
         yield return new WaitForSeconds(0.5f);
         EndCanvas.enabled = true;
-        EndCanvas.gameObject.GetComponent<Animator>().SetTrigger("EndScene");
-        yield return new WaitForSeconds(3f);
-        LevelManager.instance.loadLevelAsyncWithIndex(0);
+    }
+
+    public void returnToMainScene()
+    {
+        if (!buttonPressed)
+        {
+            buttonPressed = true;
+            LevelManager.instance.loadLevelAsyncWithIndex(0);
+        }
 
     }
 }
